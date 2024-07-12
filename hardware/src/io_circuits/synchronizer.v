b936cd9b40bfa3b7f20ce664434899f30e7ef1ee
zhm_mac <zhm1019@qq.com>
2024-07-12 15:47:53 +0800

    io_circuits

--- hardware/src/io_circuits/synchronizer.v
+++ hardware/src/io_circuits/synchronizer.v
@@ -6,6 +6,16 @@ module synchronizer #(parameter WIDTH = 1) (
   output [WIDTH-1:0] sync_signal
 );
 
-  // TODO: Your code
+  wire [WIDTH-1:0] dff0_q;
+  REGISTER #(.N(WIDTH)) dff0 (
+    .q(dff0_q),
+    .d(async_signal),
+    .clk(clk)
+  );
 
+  REGISTER #(.N(WIDTH)) dff1 (
+    .q(sync_signal),
+    .d(dff0_q),
+    .clk(clk)
+  );
 endmodule
